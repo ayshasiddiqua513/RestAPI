15c20134e43d5c7d8bbdafacc95134a726a6295d
ayshasiddiqua513 <117656371+ayshasiddiqua513@users.noreply.github.com>
2024-03-04 16:14:23 +0530

    DAY 12

---
+++ DAY 13/CE 1/springapp/src/main/java/com/example/springapp/service/AuthorService.java
@@ -0,0 +1,42 @@
+package com.example.springapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springapp.model.Author;
+import com.example.springapp.repository.AuthorRepository;
+
+@Service
+public class AuthorService {
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public Author saveAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public Author getAuthorById(Long id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author updateAuthor(Long id, Author author) {
+        Author existingAuthor = authorRepository.findById(id).orElse(null);
+        if (existingAuthor != null) {
+            existingAuthor.setEmail(author.getEmail());
+            existingAuthor.setPhoneNumber(author.getPhoneNumber());
+            existingAuthor.setAddress(author.getAddress());
+            return authorRepository.save(existingAuthor);
+        }
+        return null;
+    }
+
+    public void deleteAuthor(Long id) {
+        authorRepository.deleteById(id);
+    }
+}
